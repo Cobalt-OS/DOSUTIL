e37c384628a7da3fe45a5ca2f9a34e7de877884e
Ercan Ersoy <ercanersoy@ercanersoy.net>
2018-07-05 13:44:02 +0300

    Revert some changes
    Revert some changes for GNU Coreutils standardization.

--- dosc.h
+++ dosc.h
@@ -1,8 +1,6 @@
-/*
- * dosc.h - DOS Coreutils main header file
- * Some of the code is given below from GNU Coreutils.
- * Written the other by Ercan Ersoy and LiquidFox1776.
-*/
+/* dosc.h - DOS Coreutils main header file
+   Some of the code is given below from GNU Coreutils.
+   Written the other by Ercan Ersoy and LiquidFox1776. */
 
 #include <conio.h>
 #include <direct.h>
@@ -29,55 +27,63 @@
 #ifdef VERSION
 void version(void)
 {
-   fputs(STRING_DOS_COREUTILS_VERSION, stdout);
+   puts(STRING_DOS_COREUTILS_VERSION);
 }
 #endif
 
 enum
 {
-   GETOPT_HELP_CHAR = (CHAR_MIN - 2),
-   GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
+  GETOPT_HELP_CHAR = (CHAR_MIN - 2),
+  GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
 };
 
-#define GETOPT_HELP_OPTION_DECL "help", no_argument, NULL, GETOPT_HELP_CHAR
-#define GETOPT_VERSION_OPTION_DECL "version", no_argument, NULL, GETOPT_VERSION_CHAR
+#define GETOPT_HELP_OPTION_DECL \
+  "help", no_argument, NULL, GETOPT_HELP_CHAR
+#define GETOPT_VERSION_OPTION_DECL \
+  "version", no_argument, NULL, GETOPT_VERSION_CHAR
 
-#define case_GETOPT_HELP_CHAR  \
-   case GETOPT_HELP_CHAR:  \
-     usage (EXIT_SUCCESS);  \
-     break;
+#define case_GETOPT_HELP_CHAR                   \
+  case GETOPT_HELP_CHAR:                        \
+    usage (EXIT_SUCCESS);                       \
+    break;
+    
+#define case_GETOPT_VERSION_CHAR                        \
+  case GETOPT_VERSION_CHAR:                                             \
+    version();                                  \
+    exit (EXIT_SUCCESS);                                                \
+    break;
 
-#define case_GETOPT_VERSION_CHAR  \
-   case GETOPT_VERSION_CHAR:  \
-     version();  \
-     exit (EXIT_SUCCESS);  \
-     break;
+#define HELP_OPTION_DESCRIPTION \
+        (STRING_HELP_OPTION_DESCRIPTION)
+#define VERSION_OPTION_DESCRIPTION \
+        (STRING_VERSION_OPTION_DESCRIPTION)
 
-#define emit_try_help()  \
-   do  \
-   {  \
-      fprintf(stderr, STRING_EMIT_TRY_HELP, PROGRAM_NAME);  \
-   }  \
-   while(0)
-
-static inline void emit_stdin_note(void)
+#define emit_try_help()\
+        do\
+            {\
+               fprintf(stderr, STRING_EMIT_TRY_HELP,\
+                       PROGRAM_NAME);\
+            }\
+        while(0)
+            
+static inline void
+emit_stdin_note (void)
 {
-   fputs("", stdout);
-   fputs(STRING_EMIT_STDIN_NOTE, stdout);
+  fputs (_(STRING_EMIT_STDIN_NOTE), stdout);
 }
-
-static inline void emit_mandatory_arg_note(void)
+static inline void
+emit_mandatory_arg_note (void)
 {
-   fputs("", stdout);
-   fputs(STRING_EMIT_MANDATORY_ARG_NOTE, stdout);
+  fputs (_(STRING_EMIT_MANDATORY_ARG_NOTE), stdout);
 }
 
 #define STREQ(a, b) (strcmp (a, b) == 0)
 
 #ifndef MAX
-   #define MAX(a, b) ((a) > (b)) ? (a) : (b))
+# define MAX(a, b) ((a) > (b)) ? (a) : (b))
 #endif
 
 #ifndef MIN
-   #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+# define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #endif
+
